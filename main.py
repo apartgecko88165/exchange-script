bf6d9e349ac0b6d97d654fb35320a74b6bd3b1e0
apartgecko88165 <apartgecko88165@gmail.com>
2021-10-29 08:14:20 -0700

    no time

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import json
+
+with open("people.json", "r") as f:
+    people = json.load(f)
+
+def make(people):
+    pre = [i for i in range(len(people))]
+    post = [i for i in range(len(people))]
+
+    result = {}
+    pairing = True
+    while pairing:
+        choice = random.choice(people)
+        index = random.randint(0, len(people) - 1)
+        if choice not in pre:
+            pre[index] = choice
+        if choice not in post:
+            post[index - 1] = choice
+        total = 0
+        for i in people:
+            if i in pre and i in post:
+                total += 1
+        if total >= len(people):
+            for i in range(len(people)):
+                result[pre[i]] = post[i]
+            pairing = False
+    return result
+
+result = make(people)
+
+with open("previous.json", "r") as f:
+    pass
+
+with open("list.txt", "w") as f:
+    doc = ""
+    for i in result:
+        doc += f"{i} --> {result[i]}\n"
+    f.write(doc)
